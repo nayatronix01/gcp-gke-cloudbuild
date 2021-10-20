2c0381ebef67755ecca5403ab0d2f0a6e1e3e401
Obi NAYA-TRONIX <nayatronix@gmail.com>
2021-10-20 04:40:35 +0100

    Edited cloudbuild.yaml

--- main.tf
+++ main.tf
@@ -64,14 +64,14 @@ data "google_compute_default_service_account" "default" {
 }
 
 resource "google_service_account" "sa" {
-  account_id   = "my-service-account"
+  account_id   = "tf-service-account"
   display_name = "A service account that Terraform can use"
 }
 
 resource "google_service_account_iam_member" "admin-account-iam" {
   service_account_id = google_service_account.sa.name
   role               = "roles/iam.serviceAccountUser"
-  member             = "user:nayatronix@pm.me"
+  member             = "user:1044070725201@cloudbuild.gserviceaccount.com"
 }
 
 # Allow SA service account use the default GCE account
