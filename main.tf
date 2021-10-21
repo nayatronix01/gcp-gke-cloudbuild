6d80aefecf49162928664847cbdb914501ecd0cb
Obi NAYA-TRONIX <nayatronix@gmail.com>
2021-10-21 03:41:11 +0100

    Edited main.tf and variables.tf

--- main.tf
+++ main.tf
@@ -63,7 +63,8 @@ resource "google_cloudbuild_trigger" "service-account-trigger" {
     branch_name = ".*"
     repo_name   = "https://github.com/nayatronix01/gcp-gke-cloudbuild.git"
   }
-  
+ 
+#
   name    = "gcp-gke-cloudbuild"
   project = var.project_id
   #service_account = google_service_account.cloudbuild_service_account.id
