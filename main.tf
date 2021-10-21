268305c61226641132d84864891a3679cf6130ae
Obi NAYA-TRONIX <nayatronix@gmail.com>
2021-10-21 02:41:14 +0100

    Edited main.tf and variables.tf

--- main.tf
+++ main.tf
@@ -58,21 +58,33 @@ module "gke" {
 }
 
 
-resource "google_cloudbuild_trigger" "gcp-gke-cloudbuild" {
-  name    = format("gcp-gke-cloudbuild-%s", var.repo_name)
-  github {
-    owner = var.repo_owner
-    name  = var.repo_name
-    push {
-      branch = ".*"
-    }
+resource "google_cloudbuild_trigger" "service-account-trigger" {
+  trigger_template {
+    branch_name = "main"
+    repo_name   = "gcp-gke-cloudbuild"
   }
 
-#  filename   = "cloudbuild.yaml"
-#  provider   = google-beta
-#   provider   = google
+  service_account = google_service_account.cloudbuild_service_account.id
+  filename        = "cloudbuild.yaml"
+  depends_on = [
+    google_project_iam_member.act_as,
+    google_project_iam_member.logs_writer
+  ]
 }
 
+resource "google_service_account" "cloudbuild_service_account" {
+  account_id = "my-service-account"
+}
+
+resource "google_project_iam_member" "act_as" {
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
+
+resource "google_project_iam_member" "logs_writer" {
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
 
 #resource "google_storage_bucket" "work-examples" {
 #  name          = "work-examples project bucket"
